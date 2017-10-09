2e254ed5f7b582fbf2cd2e3c77a4747805407671
cxy <542377632@qq.com>
2017-10-09 20:53:48 +0800

    addDemo.sh

---
+++ demo.sh
@@ -0,0 +1,12 @@
+if [ -d $1 ]; then
+exit
+else
+mkdir $1
+cd $1
+mkdir css js
+echo "<!DOCTYPE>
+ <title>Hello</title>
+ <h1>Hi</h1>">index.html " h1{color: red;}">css/style.css "var string = "Hello World"
+ alert(string)">js/main.js
+exit
+fi
