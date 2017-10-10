00210aa8c21a1cc7d6e9433f0dd33589ded95b5e
cxy <542377632@qq.com>
2017-10-10 12:15:12 +0800

    revise demo.sh

--- demo.sh
+++ demo.sh
@@ -1,12 +1,13 @@
 if [ -d $1 ]; then
-exit
+  echo 'error: dir exists'
+  exit
 else
-mkdir $1
-cd $1
-mkdir css js
-echo "<!DOCTYPE>
- <title>Hello</title>
- <h1>Hi</h1>">index.html " h1{color: red;}">css/style.css "var string = "Hello World"
- alert(string)">js/main.js
-exit
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  echo "h1{color: red;}" > css/style.css
+  echo "var string = \"Hello World\"\nalert(string)" > js/main.js
+  echo 'success'
+  exit
 fi
